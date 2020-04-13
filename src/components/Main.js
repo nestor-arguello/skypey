6bfd839acd12ef9af8474b0d20087818e13d4c2c
Nestor Arguello <nestor.arguello@gmail.com>
2020-04-12 21:24:49 -0400

    add back-portfolio-btn

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,11 +2,13 @@ import React from 'react'
 import styled from 'styled-components'
 import Empty from './Empty'
 import ChatWindow from '../containers/ChatWindow'
+import BackPortfolioBtn from '../components/back-portfolio-btn/back-portfolio-btn.component';
 
 const StyledMain = styled.main`
   flex: 1;
   background-color: rgba(25, 25, 27, 1);
   height: 100%;
+  position: relative;
 `
 
 const Main = ({ user, activeUserId, ...props }) => {
@@ -16,7 +18,7 @@ const Main = ({ user, activeUserId, ...props }) => {
     ) : (
       <Empty user={user} activeUserId={activeUserId} />
     )
-  return <StyledMain>{renderMainContent}</StyledMain>
+  return <StyledMain><BackPortfolioBtn />{renderMainContent}</StyledMain>
 }
 
 export default Main
---
+++ src/components/back-portfolio-btn/back-portfolio-btn.component.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './back-portfolio-btn.styles.css';
+
+const BackPortfolioBtn = () => (
+  <div class="back-home-btn">
+    <a
+      href="https://nestor-arguello.github.io/portfolio"
+      target="_self"
+      title="Back Home"
+    >
+      &#8617;
+    </a>
+  </div>
+);
+
+export default BackPortfolioBtn;
